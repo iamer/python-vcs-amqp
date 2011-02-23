809a218b9d4558347c26ee1e65cce2cc8a5ceb3c
Ed Bartosh <bartosh@gmail.com>
2011-02-23 19:04:35 +0200

    added log sender module

---
+++ vcsamqp/senders/log.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python -tt
+
+
+class FileLogger:
+    def __init__(self, filename):
+        self._filename = filename
+
+    def send_payload(self, payload):
+    
+        with open(self._filename, "wa") as fhandler:
+            fhandler.write(str(payload))
