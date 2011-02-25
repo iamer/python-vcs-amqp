290659f1e0e1f4b380e74ca0f0a863c2d8f4cee5
Anton Berezin <anton.berezin@nokia.com>
2011-02-25 17:25:59 +0200

    Moved several methods out of the class.

--- vcsamqp/hooks/git.py
+++ vcsamqp/hooks/git.py
@@ -53,67 +53,64 @@ def _get_revlist(old, new):
         return handler.read()
 
 
+def _get_commits(old, new):
+    revlist = _get_revlist(old, new)
+    sections = revlist.split('\n\n')[:-1]
+
+    commits = []
+    index = 0
+    while index < len(sections):
+        lines = sections[index].split('\n')
+
+        # first line is 'commit HASH\n'
+        props = {'id': lines[0].strip().split(' ')[1]}
+
+        # read the header
+        for line in lines[1:]:
+            key, val = line.split(' ', 1)
+            props[key[:-1].lower()] = val.strip()
+
+        # read the commit message
+        props['message'] = sections[index+1]
+
+        # use github time format
+        basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
+        tzstr = props['date'][-5:]
+        props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
+
+        # split up author
+        author = EMAIL_RE.match(props['author'])
+        if author:
+            props['name'] = author.group(1)
+            props['email'] = author.group(2)
+        else:
+            props['name'] = 'unknown'
+            props['email'] = 'unknown'
+        del props['author']
+
+        # increment the counter
+        index += 2
+
+        commits.append({
+            'id': props['id'],
+            'author': {'name': props['name'], 'email': props['email']},
+            'message': props['message'],
+            'timestamp': props['date']
+        })
+
+    return commits
+
 
 class GitHooks:
+
     def __init__(self, sender):
         self._sender = sender
-
-    def _get_revisions(self, old, new):
-        revlist = _get_revlist(old, new)
-        sections = revlist.split('\n\n')[:-1]
-
-        revisions = []
-        s = 0
-        while s < len(sections):
-            lines = sections[s].split('\n')
-
-            # first line is 'commit HASH\n'
-            props = {'id': lines[0].strip().split(' ')[1]}
-
-            # read the header
-            for l in lines[1:]:
-                key, val = l.split(' ', 1)
-                props[key[:-1].lower()] = val.strip()
-
-            # read the commit message
-            props['message'] = sections[s+1]
-
-            # use github time format
-            basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
-            tzstr = props['date'][-5:]
-            props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
-
-            # split up author
-            m = EMAIL_RE.match(props['author'])
-            if m:
-                props['name'] = m.group(1)
-                props['email'] = m.group(2)
-            else:
-                props['name'] = 'unknown'
-                props['email'] = 'unknown'
-            del props['author']
-
-            revisions.append(props)
-            s += 2
-
-        return revisions
-
-    def _get_commits(self, old, new):
-        revisions = self._get_revisions(old, new)
-        commits = []
-        for r in revisions:
-            commits.append({'id': r['id'],
-                    'author': {'name': r['name'], 'email': r['email']},
-                    'message': r['message'],
-                    'timestamp': r['date']
-                    })
-        return commits
 
     def postreceive(self, old, new, ref):
         """Postcommit hook."""
 
         # collect the info
-        commits = self._get_commits(old, new)
+        commits = _get_commits(old, new)
 
         # prepare payload
         payload = GitHookPayload(old, new, ref, commits)
